88eac6047035d81ccc9b42abd94a3646dced2244
broton <itfootman@163.com>
2019-05-07 23:24:00 +0800

    Delete copy clang lib.

--- install.sh
+++ install.sh
@@ -7,14 +7,6 @@ ruby extconf.rb
 make clean
 make -j8
 
-if [ "$(uname)"=="Darwin" ]; then
-    cp $ROOT/bundle/youcompleteme/third_party/ycmd/libclang-6.0.0-x86_64-apple-darwin.tar.bz2 \
-       $ROOT/bundle/youcompleteme/third_party/ycmd/clang_archives
-elif [ "$(expr substr $(uname -s) 1 5)"=="Linux" ]; then
-    cp $ROOT/bundle/youcompleteme/third_party/ycmd/libclang-6.0.0-x86_64-linux-gnu-ubuntu-14.04.tar.bz2 \
-       $ROOT/bundle/youcompleteme/third_party/ycmd/clang_archives
-fi
-
 cd $ROOT/bundle/youcompleteme
 python install.py --all
 
